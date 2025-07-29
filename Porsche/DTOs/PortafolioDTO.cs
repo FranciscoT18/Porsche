594d5e9cc6d23d88264b1fb3faa126b14a6f1b8f
FranciscoT18 <169111488+FranciscoT18@users.noreply.github.com>
2025-07-28 20:57:45 -0500

    Add files via upload

--- Porsche/DTOs/PortafolioDTO.cs
+++ Porsche/DTOs/PortafolioDTO.cs
@@ -6,5 +6,8 @@
         public string Curso { get; set; } = string.Empty;
         public string? Horario { get; set; }
         public string? Profesor { get; set; }
+        public string? Title { get; set; }
+        public string? Img { get; set; }
+        public string? Aula { get; set; }
     }
 }
--- Porsche/Services/PortafolioServices.cs
+++ Porsche/Services/PortafolioServices.cs
@@ -4,9 +4,22 @@ namespace Porsche.Services
 {
     public class PortafolioServices : PortafolioInterface
     {
+        public String? UrlImg;
+        public PortafolioServices()
+        {
+            UrlImg = "https://e7.pngegg.com/pngimages/21/228/png-clipart-computer-icons-user-profile-others-miscellaneous-face.png";
+        }
+
         public PortafolioDTO GetPortafolio()
         {
             PortafolioDTO portafolioObj = new PortafolioDTO();
+            portafolioObj.NombreMateria = "Ingenieria De Software";
+            portafolioObj.Curso = "Lab: 1 JOBS";
+            portafolioObj.Horario = "10:45 A 21:00 Lunes Y Miércoles";
+            portafolioObj.Profesor = "Pablo Backgardigans";
+            portafolioObj.Title = "Ordinario 1 2025 LAS AGUAS";
+            portafolioObj.Aula = "01";
+            portafolioObj.Img = "";
             return portafolioObj;
         }
     }
